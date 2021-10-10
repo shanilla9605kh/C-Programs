8f092009fe4cc35dd490fd3a4ae5b41902d1c2df
shanilla9605kh <92257500+shanilla9605kh@users.noreply.github.com>
2021-10-10 18:29:19 +0530

    Create P1.cpp

---
+++ P1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h> 
+#include<math.h> 
+int main()
+{
+int a, r, n, sum; printf("value of a: "); scanf("%d", &a); printf("value of r: "); scanf("%d", &r); printf("value of n: "); scanf("%d", &n); if(r>1)
+{
+}
+else
+{
+sum=a*((1-pow(r,n))/(1-r));
+}
+printf("Sum of geometric progression is: %d ", sum);
+}
