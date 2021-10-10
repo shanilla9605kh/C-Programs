2ad0ad0a8e154c2d0dbe48340b104c0e1812c9e4
shanilla9605kh <92257500+shanilla9605kh@users.noreply.github.com>
2021-10-10 18:30:29 +0530

    Create P3.cpp

---
+++ P3.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void towers(int, char, char, char);
+
+int main()
+{
+int num;
+
+printf("Enter the number of disks : "); scanf("%d", &num);
+printf("The Sequence of moves involved in the Tower of Hanoi are :\n"); towers(num, 'A', 'C', 'B');
+return 0;
+}
+void towers(int num, char frompeg, char topeg, char auxpeg)
+{
+if (num == 1)
+{
+printf("\n Move disk 1 from peg %c to peg %c", frompeg, topeg); return;
+}
