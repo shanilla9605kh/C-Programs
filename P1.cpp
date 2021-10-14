d2ec9c427c4bd0f18189d2f1ffc0584bac08520b
rb46723 <92258711+rb46723@users.noreply.github.com>
2021-10-14 14:27:43 +0530

    Update P1.cpp

--- P1.cpp
+++ P1.cpp
@@ -1,13 +1,20 @@
-#include <stdio.h> 
-#include<math.h> 
+#include <stdio.h>
+#include <math.h>
 int main()
 {
-int a, r, n, sum; printf("value of a: "); scanf("%d", &a); printf("value of r: "); scanf("%d", &r); printf("value of n: "); scanf("%d", &n); if(r>1)
-{
-}
-else
-{
-sum=a*((1-pow(r,n))/(1-r));
-}
-printf("Sum of geometric progression is: %d ", sum);
+    int a, r, n, sum;
+    printf("value of a: ");
+    scanf("%d", &a);
+    printf("value of r: ");
+    scanf("%d", &r);
+    printf("value of n: ");
+    scanf("%d", &n);
+    if (r > 1)
+    {
+    }
+    else
+    {
+        sum = a * ((1 - pow(r, n)) / (1 - r));
+    }
+    printf("Sum of geometric progression is: %d ", sum);
 }
